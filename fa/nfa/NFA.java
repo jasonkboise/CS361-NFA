c98b7fa78108ee7c7085914f999ced89051b3d9e
jasonkboise <54860002+jasonkboise@users.noreply.github.com>
2021-11-12 15:06:14 -0700

    Added implemented NFAState. Started NFA

---
+++ fa/nfa/NFA.java
@@ -0,0 +1,123 @@
+package fa.nfa;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import fa.State;
+import fa.dfa.DFA;
+
+public class NFA implements NFAInterface{
+    private Set<NFAState> states;
+    private NFAState start;
+    private Set<Character> ordAbc;
+
+    public NFA() {
+        states = new LinkedHashSet<NFAState>();
+        ordAbc = new LinkedHashSet<Character>();
+    }
+
+    @Override
+    public void addStartState(String name) {
+        NFAState s = checkIfExists(name);
+        if (s == null) {
+            s = new NFAState(name);
+            addState(s);
+        }
+        else {
+            System.out.println("WARNING: A state with name " + name + " already exists in the DFA");
+        }
+        start = s;
+    }
+
+    @Override
+    public void addState(String name) {
+        NFAState s = checkIfExists(name);
+        if (s == null) {
+            s = new NFAState(name);
+            addState(s);
+        }
+        else {
+            System.out.println("WARNING: A state with name " + name + " already exists in the DFA");
+        }
+    }
+    private void addState(NFAState s){
+		states.add(s);
+	}
+
+    @Override
+    public void addFinalState(String name) {
+        NFAState s = checkIfExists(name);
+        if (s == null) {
+            s = new NFAState(name, true);
+            addState(s);
+        }
+        else {
+            System.out.println("WARNING: A state with name " + name + " already exists in the DFA");
+        }
+    }
+
+    @Override
+    public void addTransition(String fromState, char onSymb, String toState) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Set<? extends State> getStates() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<? extends State> getFinalStates() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public State getStartState() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<Character> getABC() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public DFA getDFA() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<NFAState> getToState(NFAState from, char onSymb) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<NFAState> eClosure(NFAState s) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+	 * Check if a state with such name already exists
+	 * @param name
+	 * @return null if no state exist, or DFAState object otherwise.
+	 */
+	private NFAState checkIfExists(String name){
+		NFAState ret = null;
+		for(NFAState s : states){
+			if(s.getName().equals(name)){
+				ret = s;
+				break;
+			}
+		}
+		return ret;
+	}
+    
+}
---
+++ fa/nfa/NFAState.java
@@ -0,0 +1,77 @@
+package fa.nfa;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import fa.State;
+
+public class NFAState extends State {
+
+    private HashMap<Character,LinkedList<NFAState>> delta;//delta
+	private boolean isFinal;//remembers its type
+    
+    /**
+	 * Default constructor
+	 * @param name the state name
+	 */
+    public NFAState(String name) {
+        initDefault(name);
+        isFinal = false;
+    }
+
+    public NFAState(String name, boolean isFinal) {
+        initDefault(name);
+        this.isFinal = isFinal;
+    }
+
+    /**
+	 * Overlaoded constructor that sets the state type
+	 * @param name the state name
+	 * @param isFinal the type of state: true - final, false - nonfinal.
+	 */
+    private void initDefault(String name) {
+        this.name = name;
+        delta = new HashMap<Character,LinkedList<NFAState>>();
+    }
+
+    /**
+	 * Accessor for the state type
+	 * @return true if final and false otherwise
+	 */
+    public boolean isFinal() {
+        return isFinal;
+    }
+
+    /**
+	 * Add the transition from <code> this </code> object
+	 * @param onSymb the alphabet symbol
+	 * @param toState to NFA state
+	 */
+	public void addTransition(char onSymb, NFAState toState){
+        LinkedList<NFAState> states = delta.get(onSymb);
+        if (states == null) {
+            states = new LinkedList<NFAState>();
+            states.add(toState);
+            delta.put(onSymb, states);
+        }
+        else {
+            states.add(toState);
+        }
+	}
+
+    /**
+	 * Retrieves the state that <code>this</code> transitions to
+	 * on the given symbol
+	 * @param symb - the alphabet symbol
+	 * @return the set of new states 
+	 */
+	public LinkedList<NFAState> getTo(char symb){
+		LinkedList<NFAState> ret = delta.get(symb);
+		if(ret == null){
+			 System.err.println("ERROR: DFAState.getTo(char symb) returns null on " + symb + " from " + name);
+			 System.exit(2);
+			}
+		return delta.get(symb);
+	}
+
+}
