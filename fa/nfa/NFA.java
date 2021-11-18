cdaf6061319aed9646e7232cff272bccd0ecbf90
Connor Jackson <connorjackson@u.boisestate.edu>
2021-11-17 19:14:43 -0700

    add javadoc

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -10,6 +10,17 @@ import java.util.Set;
 import fa.State;
 import fa.dfa.DFA;
 
+/**
+ * This is the class that contains the methods
+ * for setting up a Non Deterministic Finite
+ * Automata. The methods include adding states 
+ * (start, final, and neither), creating the 
+ * alphabet, getDFA, and adding transitions.
+ * 
+ * @author Jason Kuphaldt, Connor Jackson
+ *
+ */
+
 public class NFA implements NFAInterface{
     private Set<NFAState> states;
     
@@ -21,6 +32,12 @@ public class NFA implements NFAInterface{
         ordAbc = new LinkedHashSet<Character>();
     }
 
+    
+    /** 
+     * @param name is a string that start is
+     * set to. It is also added to the set of
+     * states. 
+     */
     @Override
     public void addStartState(String name) {
         NFAState s = checkIfExists(name);
@@ -34,6 +51,13 @@ public class NFA implements NFAInterface{
         start = s;
     }
 
+    
+    /** 
+     * @param name is the string that 
+     * is set as a state which means it
+     * is added to the set of states for
+     * the NFA.
+     */
     @Override
     public void addState(String name) {
         NFAState s = checkIfExists(name);
@@ -45,10 +69,20 @@ public class NFA implements NFAInterface{
             System.out.println("WARNING: A state with name " + name + " already exists in the DFA");
         }
     }
+    
+    /** 
+     * @param s Just as addState above.
+     */
     private void addState(NFAState s){
 		states.add(s);
 	}
 
+    
+    /** 
+     * @param name is a string added to 
+     * the set of finalstate and also 
+     * to the set of states. 
+     */
     @Override
     public void addFinalState(String name) {
         NFAState s = checkIfExists(name);
@@ -61,6 +95,17 @@ public class NFA implements NFAInterface{
         }
     }
 
+    
+    /** 
+     * @param fromState is a string of the beginning state.
+     * @param onSymb is the character that represents the
+     * transition symbol.
+     * @param toState is the sstring that represents the
+     * state that will be transitioned to based on the onsymb
+     * 
+     * This method recieves 3 values and adds them as transition 
+     * data to the fromState. 
+     */
     @Override
     public void addTransition(String fromState, char onSymb, String toState) {
         checkIfExists(fromState).addTransition(onSymb,checkIfExists(toState));
@@ -70,11 +115,19 @@ public class NFA implements NFAInterface{
         
     }
 
+    
+    /** 
+     * @return Set<NFAState>
+     */
     @Override
     public Set<NFAState> getStates() {
         return states;
     }
 
+    
+    /** 
+     * @return Set<NFAState>
+     */
     @Override
     public Set<NFAState> getFinalStates() {
 		Set<NFAState> ret = new LinkedHashSet<NFAState>();
@@ -86,16 +139,32 @@ public class NFA implements NFAInterface{
 		return ret;
 	}
 
+    
+    /** 
+     * @return start State
+     */
     @Override
     public State getStartState() {
         return start;
     }
 
+    
+    /** 
+     * @return Set<Character>
+     */
     @Override
     public Set<Character> getABC() {
         return ordAbc;
     }
 
+    
+    /** 
+     * @param from the beginning state
+     * @param onSymb the transition symbol
+     * @return the state that will be 
+     * transitioned to based on the 
+     * transition data.
+     */
     private Set<NFAState> getToState(Set<NFAState> from, char onSymb) {
 		Set<NFAState> ret = new LinkedHashSet<NFAState>();
 
@@ -106,6 +175,16 @@ public class NFA implements NFAInterface{
 		return ret;
 	}
 
+    
+    /** 
+     * @return DFA
+     * Takes the NFA and turns it into a DFA.
+     * Creates new empty NFA set, puts
+     * states into the set. Iterates through
+     * the set and turns it into a dfa. The dfa
+     * is returned.
+     * -ALL NFAs are Also DFAs-
+     */
     @Override
     public DFA getDFA() {
         DFA dfa = new DFA();
@@ -146,6 +225,14 @@ public class NFA implements NFAInterface{
 		return dfa;
     }
 
+    
+    /** 
+     * @param states
+     * @return boolean
+     * Iterates through set of NFA states
+     * and returns true for every state
+     * that is also a final state
+     */
     private boolean containsFinalState(Set<NFAState> states) {
         boolean ret = false;
         for(NFAState state: states){
@@ -157,11 +244,24 @@ public class NFA implements NFAInterface{
         return ret;
     }
 
+    
+    /** 
+     * @param from The start state as a NFAState object
+     * @param onSymb The transition symbol as a char
+     * @return Set<NFAState>
+     */
     @Override
     public Set<NFAState> getToState(NFAState from, char onSymb) {
         return from.getTo(onSymb);
     }
 
+    
+    /** 
+     * @param s is a NFAState object. 
+     * Creates new hashset. adds s to hashet.
+     * returns visted.
+     * @return Set<NFAState>
+     */
     @Override
     public Set<NFAState> eClosure(NFAState s) {
         Set<NFAState> visited = new LinkedHashSet<NFAState>();
@@ -169,6 +269,12 @@ public class NFA implements NFAInterface{
         return visited;
     }
 
+    
+    /** 
+     * @param s Is a set of NFAState objects
+     * Runs eclosure on every NFAState in the set.
+     * @return Set<NFAState>
+     */
     public Set<NFAState> eClosure(Set<NFAState> s) {
         Set<NFAState> ret = new LinkedHashSet<NFAState>();
 		for (NFAState nfaState : s) {
@@ -178,6 +284,12 @@ public class NFA implements NFAInterface{
 		return ret;
     }
 
+    
+    /** 
+     * @param s NFA State object
+     * @param visited Set of NFAStates
+     * @return Set<NFAState>
+     */
     private Set<NFAState> eClosure(NFAState s, Set<NFAState> visited) {
         if (!visited.contains(s)) {
             visited.add(s);
@@ -192,7 +304,7 @@ public class NFA implements NFAInterface{
 
     /**
 	 * Check if a state with such name already exists
-	 * @param name
+	 * @param name 
 	 * @return null if no state exist, or DFAState object otherwise.
 	 */
 	private NFAState checkIfExists(String name){
